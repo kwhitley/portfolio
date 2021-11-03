762127c399ed148c41ff380f88f0eb925c1da94a
Kevin Whitley <krwhitley@gmail.com>
2021-11-03 14:49:37 -0500

    updated splash with Twitter link

--- src/App.svelte
+++ src/App.svelte
@@ -5,24 +5,44 @@
 <style type="scss">
   h1 {
     font-size: clamp(2rem, 10vmin, 5rem);
+    white-space: nowrap;
+  }
+
+  h2 {
+    text-align: center;
   }
 
   main {
     margin: 0 1em 12vh;
-  }
-
-  small {
-    font-size: clamp(0.9rem, 0.35em, 12rem);
+    position: relative;
   }
 
   em {
-    color: #f0c;
+    color: var(--accent-color);
+    position: relative;
+    display: inline-block;
   }
+
+
+  p {
+    position: absolute;
+    color: var(--foreground-color);
+    font-size: clamp(0.7rem, 2.5vmin, 1rem);
+    left: 0;
+    top: 100%;
+    letter-spacing: -0.01em;
+    text-transform: uppercase;
+    white-space: nowrap;
+  }
+
+
+
 </style>
 
 <main>
   <h1>
-    <small>welcome to</small>
     <em>swarm</em>.trade
   </h1>
+
+  <p>by <a href="https://twitter.com/kevinrwhitley">@kevinrwhitley</a></p>
 </main>
